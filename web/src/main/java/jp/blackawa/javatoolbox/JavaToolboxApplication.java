7d23c34c7300227cd02f30067f7bb6ffc9e6d9c7
blackawa <3104kurokawa@gmail.com>
2018-03-15 17:18:30 +0900

    :seedling:

---
+++ web/src/main/java/jp/blackawa/javatoolbox/JavaToolboxApplication.java
@@ -0,0 +1,12 @@
+package jp.blackawa.javatoolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaToolboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaToolboxApplication.class, args);
+	}
+}
---
+++ web/src/main/java/jp/blackawa/javatoolbox/controller/TopController.java
@@ -0,0 +1,14 @@
+package jp.blackawa.javatoolbox.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "")
+public class TopController {
+    @GetMapping("")
+    public String index() {
+        return "top/index";
+    }
+}
