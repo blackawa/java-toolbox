d771e558d3ca152712904df30b924fdf15519b4e
blackawa <3104kurokawa@gmail.com>
2018-03-15 18:39:28 +0900

    :sparkles: show search pge

---
+++ src/main/java/jp/blackawa/javatoolbox/web/controller/SearchController.java
@@ -0,0 +1,18 @@
+package jp.blackawa.javatoolbox.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = "search")
+public class SearchController {
+    @GetMapping("")
+    public String index(@RequestParam(required = false) String query) {
+        if (query == null) {
+            return "redirect:/";
+        }
+        return "search/index";
+    }
+}
